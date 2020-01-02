ce6d2844283bd4bee74c8e2deeff2b75c05e521f
Alessio Signorini <alessio@signorini.us>
2020-01-02 12:39:00 -0800

    Look also at ENVs for both OAuth2 secret and client

--- lib/huginn_fitbit_agent/fitbit_agent.rb
+++ lib/huginn_fitbit_agent/fitbit_agent.rb
@@ -11,14 +11,18 @@ module Agents
     description <<-MD
       Create events from Fitbit activities
 
-      It will remember some (`loopback`=50) activities so it can update them
-      instead of creating new ones.
+      OAUTHv2 client and secret can be specified in the configuration of the
+        agent or globally using `ENV['FITBIT_OAUTH2_CLIENT']` and
+        `ENV['FITBIT_OAUTH2_SECRET']`.
+
+      The agent will remember some (`loopback`=50) activities so it can update
+        them instead of creating new ones.
 
       Refer to [this page](https://dev.fitbit.com/build/reference/web-api/activity/#get-activity-logs-list)
-      for a list of properties of the events created.
+        for a list of properties of the events created.
 
-      Follow [these instructions](https://dev.fitbit.com/apps/oauthinteractivetutorial?clientEncodedId=YOURCLIENT&clientSecret=YOURSECRET&redirectUri=https://localhost:3000/fitbit/callback&applicationType=PERSONAL)
-      to get your first `refresh_token`.
+      After the agent is created go into its description to complete the OAUTHv2
+        flow and authorize it to read your Fitbit data.
     MD
 
     form_configurable :scope
@@ -28,8 +32,8 @@ module Agents
 
     def default_options
       {
-        'lookback' => 50,
-        'scope'    => 'activity heartrate location nutrition profile settings sleep social weight'
+        'lookback'      => 50,
+        'scope'         => 'activity heartrate location nutrition profile settings sleep social weight'
       }
     end
 
@@ -38,12 +42,12 @@ module Agents
         errors.add(:base, ":lookback needs to be a number greater than 0")
       end
 
-      unless options['oauth2_client'].present? && interpolated['oauth2_client'].match(/^[A-Z0-9]+$/)
-        errors.add(:base, ":oauth2_client needs to be defined")
+      unless oauth2_client_string.present? && oauth2_client_string.match(/^[A-Z0-9]+$/)
+        errors.add(:base, "ENV['FITBIT_OAUTH2_CLIENT'] or :oauth2_client need to be defined")
       end
 
-      unless options['oauth2_secret'].present? && interpolated['oauth2_secret'].match(/^\w+$/)
-        errors.add(:base, ":oauth2_secret needs to be defined")
+      unless oauth2_secret_string.present? && oauth2_secret_string.match(/^\w+$/)
+        errors.add(:base, "ENV['FITBIT_OAUTH2_SECRET'] or :oauth2_secret config need to be defined")
       end
     end
 
@@ -102,8 +106,19 @@ module Agents
     private
 
 
+    def oauth2_client_string
+      ENV['FITBIT_OAUTH2_CLIENT'] || interpolated['oauth2_client']
+    end
+
+
+    def oauth2_secret_string
+      ENV['FITBIT_OAUTH2_SECRET'] || interpolated['oauth2_secret']
+    end
+
+
+
     def oauth2_client
-      OAuth2::Client.new(interpolated['oauth2_client'],interpolated['oauth2_secret'],
+      OAuth2::Client.new(oauth2_client_string,oauth2_secret_string,
         :site           => 'https://api.fitbit.com',
         :authorize_url  => '/oauth2/authorize',
         :token_url      => '/oauth2/token',
