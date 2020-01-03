9a64deca3a6381b412ad471072a87ec76091e43e
Alessio Signorini <alessio@signorini.us>
2020-01-02 23:58:14 -0800

    HuginnFitbitAgent: declared Rails::Engine and added _show partial

--- lib/huginn_fitbit_agent.rb
+++ lib/huginn_fitbit_agent.rb
@@ -1,4 +1,6 @@
 require 'huginn_agent'
 
+class Engine < ::Rails::Engine; end
+
 #HuginnAgent.load 'huginn_fitbit_agent/concerns/my_agent_concern'
 HuginnAgent.register 'huginn_fitbit_agent/fitbit_agent'
